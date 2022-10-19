0390f7c97927376fc97dce570507b7e75c2d52e2
m253516 <115736461+m253516@users.noreply.github.com>
2022-10-19 14:20:53 -0400

    day 2

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,14 +1,18 @@
 import sys
+
 import pygame
 
+from setting import Settings
 class AlienInvasion:
-    """Overall class to manage game assets behaviour"""
+    """Overall class to manage game assets and behaviour."""
 
     def __init__(self):
         """Initialize the game, and create game resources."""
         pygame.init()
+        self.settings = Settings()
 
-        self.screen = pygame.display.set_mode((1200,800))
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
 
     def run_game(self):
@@ -41,4 +45,3 @@ if __name__ == '__main__':
             self.screen.fill(self.bg_color)
             # Make the most recently drawn screen visible
             pygame.display.flip()
-
