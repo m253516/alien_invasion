62e19388f3bb65450796716cf4125291dd077212
m253516 <115736461+m253516@users.noreply.github.com>
2022-11-11 00:26:21 -0500

    page 264 (full fleet and it runs)
    It does not move.

--- alien_invasion.py
+++ alien_invasion.py
@@ -99,7 +99,9 @@ class AlienInvasion:
 
         # Determine the number of rows of aliens that fit on the screen.
         ship_height = self.ship.rect.height
-        available_spce_y = (self.settings.screen height - (3 * alien_height) - ship_height)
+        available_space_y = (self.settings.screen_height -
+                                (3 * alien_height) - ship_height)
+        number_rows = available_space_y // (2 * alien_height)
 
         # Create the full fleet of aliens.
         for row_number in range(number_rows):
@@ -112,9 +114,9 @@ class AlienInvasion:
         alien_width, alien_height = alien.rect.size
         alien.x = alien_width + 2 * alien_width * alien_number
         alien.rect.x = alien.x
-        alien.rect.x = alien.rect.height + 2 * alien.rect.height * row_number
+        alien.rect.y = alien.rect.height + 2 * alien.rect.height * row_number
         self.aliens.add(alien)
-        
+
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen."""
         self.screen.fill(self.bg_color)
